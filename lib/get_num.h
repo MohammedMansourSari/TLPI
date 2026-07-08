79c376cff54c997babdd44cbd36678bf10518fba
Mohammed Mansour <nyxvaleorenth@proton.me>
2026-07-08 10:25:36 +0300

    commit

---
+++ lib/get_num.h
@@ -0,0 +1,15 @@
+#ifndef GET_NUM_H
+#define GET_NUM_H
+
+#define GN_NONNEG 01    /* value must be >= 0 */
+#define GN_GT_0 02      /* value must be >= 0 */
+
+#define GN_ANY_BASE 01000
+#define GN_BASE_8   02000
+#define GN_BASE_16  04000
+
+long getLong(const char *arg, int flags, const char *name);
+
+int getInt(const char *arg, int falgs, const char *name);
+
+#endif
